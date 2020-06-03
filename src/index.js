29ab1c8018fb28fb0c4d5f3669648801eb6becfb
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-06-03 18:52:22 -0500

    extraer los links
    usando marked

--- src/index.js
+++ src/index.js
@@ -1,8 +1,104 @@
-// const fs = require ('fs');
-const os = require ('os');
-
-let cpu = os.cpus();
-console.log(cpu);
-
+const fs = require ('fs');
+const path = require ('path');
+const marked = require ('marked');
+const renderer = new marked.Renderer();
 
 
+// funcion para saber si la ruta es valida - boolean
+const checkRoute = (route) => {
+    const routeIsValid = fs.existsSync(route);
+    if (!routeIsValid) {
+      return ('La ruta ingresada no es válida, intente de nuevo por favor');
+    }
+    return routeIsValid;
+  };
+   console.log('ruta ingresada valida', checkRoute('./README.md'));
+
+// función para saber si ruta es absoluta - boolean
+const checkAbsolutePath = (route) => path.isAbsolute(route);
+console.log('la ruta es absoluta', checkAbsolutePath('./README.md'));
+
+// función para transformar ruta relativa a absoluta
+const routeAbsolute = (route) => (path.isAbsolute(route) === true ? route : (path.resolve(route)));
+console.log('Transformando la ruta', routeAbsolute('./README.md'));
+
+// verificar si es un archivo - boolean
+const isFile = (route) => fs.statSync(route).isFile();
+console.log('es un archivo', isFile('./README.md'));
+
+// para leer un archivo sincrono
+const readFiles = (doc) => fs.readFileSync(doc, 'utf8');
+console.log('leyendo elementos de un archivo', readFiles('./src/prueba.md'))
+
+// verificar si es un directorio -boolean *
+const isDirectory = (route) => fs.lstatSync(route).isDirectory();
+console.log('es un directorio', isDirectory('./README.md'));
+
+//lee los elementos del directorio
+const readDirectory = (route) => fs.readdirSync(route);
+console.log('leyendo elementos del directorio', readDirectory('./src'));
+
+// Función para identificar que tipo de extencion es la ruta/directorio
+const routeExtension = (route) => path.extname(route);
+console.log('la extencion del archivo es', routeExtension('./test.spec.js'));
+
+//array de las rutas de los elementos del directorio **
+const getArrayFilesAndDir = (route) => {
+    return readDirectory(route).map(element =>
+      path.join(route, element),); //string
+  };
+console.log('elementos del directorio con su ruta', getArrayFilesAndDir('./src'));
+
+
+
+// funcion que guarda en un array los files con extension .md (con su ruta)
+const getFileMD = (route) =>{
+   let arrFileMD = [];
+    if (isFile(route)) {
+        if (routeExtension(route) === '.md'){
+            arrFileMD.push(route);
+        }
+    } else  {
+      getArrayFilesAndDir(route).forEach((element) => {
+        const fileNewRoute = path.join(element);
+        const getMDfileNewRoute = getFileMD(fileNewRoute);
+        arrFileMD = arrFileMD.concat(getMDfileNewRoute);
+      });
+    };
+    return arrFileMD;
+};
+console.log ('Array exclusivo de file .md', getFileMD('./src'));
+
+
+// extraer los link de los file.md 
+const extractLinkFile = (route) => {
+   const arrLinks = [];
+   getFileMD(route).forEach((file) => {
+     renderer.link = (href, title, text) => {
+       const link = {
+         href,
+         text,
+         file
+       };
+       arrLinks.push(link);
+     };
+     //convierte el contenido en html y se usa el render para buscar 
+     marked(readFiles(file), { renderer });
+   });
+   return arrLinks;
+ };
+console.log('extraer link de file', extractLinkFile('./src/prueba.md'));
+
+module.exports = {
+    checkRoute,
+    checkAbsolutePath,
+    routeAbsolute,
+    isFile,
+    readFiles,
+    isDirectory,
+    readDirectory,
+    routeExtension,
+    getArrayFilesAndDir,
+    getFileMD,
+    extractLinkFile
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,10 +1,118 @@
-describe('Verificar si la ruta es valida', () => {
-  it('Deberia verificar si la ruta ingresada es valida', () => {
-    checkRoute('./some/example.md').then((route) => {
-      expect('./some/example.md').tobe('rutavalida')
-      console.log('hola')
-    })
-  })
-})
+const path = require ('path');
 
-describe ()
+const {
+  checkRoute, checkAbsolutePath, routeAbsolute,  isFile, readFiles, isDirectory,
+  readDirectory,  routeExtension,  getArrayFilesAndDir, getFileMD, extractLinkFile
+} = require ('../src/index.js')
+
+
+describe('Verificar si la ruta es valida', () => {
+  it('Es una funcion', () => {
+      expect (typeof checkRoute).toBe('function');
+  });
+  it('Debe verificar si la ruta ingresada es valida', () => {
+    expect(checkRoute('./README.md')).toBe(true);
+});
+});
+
+
+describe('Verficar si la ruta es absoluta', () => {
+  it('Es una funcion', () => {
+      expect (typeof checkAbsolutePath).toBe('function');
+  });
+  it('Debe retornar true si la ruta es absoluta', () => {
+    expect(checkAbsolutePath('C:\\Users\\Lourdes\\Documents\\GitHub\\LIM012-fe-md-links\\README.md')).toBe(true);
+});
+  it('Debe retornar false si la ruta no es absoluta', () => {
+    expect(checkAbsolutePath('./README.md')).toBe(false);
+});
+});
+
+
+describe('Transformar la ruta a abosulta', () => {
+  it('Es una funcion', () => {
+      expect (typeof routeAbsolute).toBe('function');
+  });
+  it('Debe transformar la ruta a absoluta', () => {
+    expect(routeAbsolute('./README.md')).toBe('C:\\Users\\Lourdes\\Documents\\GitHub\\LIM012-fe-md-links\\README.md');
+});
+});
+
+
+describe('Verificar si es un file', () => {
+  it('Es una funcion', () => {
+      expect (typeof isFile).toBe('function');
+  });
+  it('Debe verificar si la ruta ingresada es un archivo: true', () => {
+    expect(isFile('./README.md')).toBe(true);
+});
+});
+
+
+describe('Identificar los elementos del file', () => {
+  it('Es una funcion', () => {
+      expect (typeof readFiles).toBe('function');
+  });
+  it('Debe identificar los elementos del file', () => {
+    expect(readFiles(path.join('./src/hola.md'))).toEqual('**Hola MUNDO**');
+});
+});
+
+
+describe('Verificar si es un directorio', () => {
+  it('Es una funcion', () => {
+      expect (typeof isDirectory).toBe('function');
+  });
+  it('Debe verificar si la ruta ingresada es un directorio: false', () => {
+    expect(isDirectory('./src')).toBe(true);
+});
+});
+
+
+describe('Identificar los elementos del directorio', () => {
+  it('Es una funcion', () => {
+      expect (typeof readDirectory).toBe('function');
+  });
+  it('Debe identificar los elementos del directorio', () => {
+    const identDir = ['cli.js', 'hola.md', 'index.js', 'prueba.md']
+    expect(readDirectory(path.join('./src'))).toEqual(identDir);
+});
+});
+
+
+describe('Identificar el tipo de extension', () => {
+  it('Es una funcion', () => {
+      expect (typeof routeExtension).toBe('function');
+  });
+  it('Debe identificar el tipo de extension del archivo', () => {
+    expect(routeExtension('./README.md')).toBe('.md');
+});
+});
+
+
+describe('Identificar las rutas de los elementos del directorio', () => {
+  it('Es una funcion', () => {
+      expect (typeof getArrayFilesAndDir).toBe('function');
+  });
+  it('Debe identificar las rutas de los elementos del directorio', () => {
+    const outputDir = ['src\\cli.js', 'src\\hola.md', 'src\\index.js', 'src\\prueba.md']
+    expect(getArrayFilesAndDir(path.join('./src'))).toEqual(outputDir);
+});
+});
+
+
+describe('Guardar en un array los files con extension .md', () => {
+  it('Es una funcion', () => {
+      expect (typeof getFileMD).toBe('function');
+  });
+  it('Debe guardar los file md en un array', () => {
+    const outputfilemd = [ 'src\\hola.md', 'src\\prueba.md' ]
+    expect(getFileMD(path.join('./src'))).toEqual(outputfilemd);
+});
+});
+
+describe('Extraer los links de los file .md', () => {
+  it('Es una funcion', () => {
+      expect (typeof getFileMD).toBe('function');
+  });
+});
